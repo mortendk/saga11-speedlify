1a13066a7a0c27edf036545140e02b38242ff2fa
mortendk <morten@thememachine.io>
2022-09-20 13:14:49 +0200

    new setup

---
+++ _data/sites/saga11.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "theme machine", // optional, falls back to object key
+  description: "Theme Machine websites",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: ["https://saga11.dev/", "https://grunn.saga11.dev/", "https://nakinn.saga11.dev/"],
+};
