c5ba2f10e1cab3b6e99d1e0403725dc2615e4836
mortendk <morten@thememachine.io>
2022-09-21 10:12:40 +0200

    Create cms.js

---
+++ _data/sites/cms.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "CMS", // optional, falls back to object key
+  description: "CMS speed",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    "https://drupal.org/",
+    "https://wordpress.com/",
+    "https://www.joomla.org/",
+    "https://craftcms.com/",
+    "https://umbraco.com/",
+    "https://typo3.org/",
+    "https://www.netlifycms.org/",
+    "https://www.siteleaf.com/",
+    "https://saga11.dev/",
+  ],
+};
